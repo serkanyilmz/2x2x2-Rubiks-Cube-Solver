0ae0d7e33f1b02c07bdc3ded8744d25f1a9686f8
Serkan Yılmaz <110226180+serkanyilmz@users.noreply.github.com>
2022-12-25 18:31:36 +0300

    Add files via upload

---
+++ colours_and_language.py
@@ -0,0 +1,23 @@
+#This file contains all the text that will seen in tkinter.
+
+surfaces_list = [[1, 3], [3, 1], [3, 3], [3, 5], [3, 7], [5, 3]]
+colour_list=["yellow","red","green","orange","blue","white"]
+
+colours={"default":"grey",#grey
+         "green":"#149414",
+         "red":"#B90000",
+         "white":"#FFFFFF",
+         "yellow":"#FFD500",
+         "orange":"#FF5800",
+         "blue":"#0045AD",
+         2:"#149414",
+         1:"#B90000",
+         5:"#FFFFFF",
+         0:"#FFD500",
+         3:"#FF5800",
+         4:"#0045AD",
+         "clear":"grey"}
+lang={"title":"Cube Solver",
+      "error_message":"There must be 4 {} squares!",
+      "while_solving":"Trying to find...",
+      "already":"Already solved!"}
